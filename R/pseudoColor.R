4484ee6c46eb6fb4b289dab286a7f977975fbf30
tkatsuki <takeo.katsuki@gmail.com>
2016-07-19 16:50:53 -0700

    Added pseudoColor

--- R/pseudoColor.R
+++ R/pseudoColor.R
@@ -1,4 +1,13 @@
-pseudoColor <- function (x, min = 0, max = 255) 
+#' Moment calculation
+#'
+#'
+#' @param x An Image object or a matrix.
+#' @param min Minimum color range.
+#' @param max Maximum color range.
+#' @export
+#' @examples
+#' pseudoColor()
+pseudoColor <- function (x, min = 0, max = 255)
 {
   x <- x * 255 + 1
   up <- seq(0, 255, by = round(256 * 4/(max - min + 1)))
@@ -6,23 +15,23 @@ pseudoColor <- function (x, min = 0, max = 255)
   half <- round((max - min - 1)/2)
   quat <- round((max - min - length(down) - 1)/2)
   if (max == 255) {
-    lut.r <- c(seq(0, min, by = 1), rep(0, half), up, 
+    lut.r <- c(seq(0, min, by = 1), rep(0, half), up,
                rep(255, (max - min - half - length(up))))
     x.r <- matrix(lut.r[x], nrow(x), ncol(x))
     lut.g <- c(seq(0, min, by = 1), up, rep(255, (max - min - length(up) * 2)), down)
     x.g <- matrix(lut.g[x], nrow(x), ncol(x))
-    lut.b <- c(seq(0, min, by = 1), rep(255, quat), down, 
+    lut.b <- c(seq(0, min, by = 1), rep(255, quat), down,
                rep(0, (max - min - length(down) - quat)))
     x.b <- matrix(lut.b[x], nrow(x), ncol(x))
   }
   else {
-    lut.r <- c(seq(0, min, by = 1), rep(0, half), up, 
+    lut.r <- c(seq(0, min, by = 1), rep(0, half), up,
                rep(255, (max - min - half - length(up) - 1)), rep(255, 255 - max))
     x.r <- matrix(lut.r[x], nrow(x), ncol(x))
-    lut.g <- c(seq(0, min, by = 1), up, 
+    lut.g <- c(seq(0, min, by = 1), up,
                rep(255, (max - min - length(up) * 2 - 1)), down, rep(0, 255 - max))
     x.g <- matrix(lut.g[x], nrow(x), ncol(x))
-    lut.b <- c(seq(0, min, by = 1), rep(255, quat), down, 
+    lut.b <- c(seq(0, min, by = 1), rep(255, quat), down,
                rep(0, (max - min - length(down) - 1 - quat)), rep(0, 255 - max))
     x.b <- matrix(lut.b[x], nrow(x), ncol(x))
   }
