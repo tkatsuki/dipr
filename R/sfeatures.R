eec4c07fc7591aca52a5197289399ac87b5b067e
tkatsuki <takeo.katsuki@gmail.com>
2016-07-19 06:44:30 -0700

    Added docs

--- R/sfeatures.R
+++ R/sfeatures.R
@@ -1,3 +1,12 @@
+#' Moment calculation
+#'
+#'
+#' @param obj A target image of Image object or an array.
+#' @param ref A reference image of Image object or an array.
+#' @export
+#' @examples
+#' sfeatures()
+
 sfeatures <- function(obj, ref=0){
   if(length(ref)!=1 & !identical(dim(obj), dim(ref))) stop("Dimension mismatch.")
   width <- dim(obj)[1]
--- R/sweepC.R
+++ R/sweepC.R
@@ -1,3 +1,12 @@
+#' Fast sweep function
+#'
+#' @param obj A target image of Image object or an array.
+#' @param ref A reference image of Image object or an array.
+#' @param op An operator.
+#' @export
+#' @examples
+#' ssweep()
+
 ssweep <- function(obj, ref, op){
   if(op == "-") {
     sweepC(obj, ref, dim(obj), 1)
