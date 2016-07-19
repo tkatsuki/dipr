e1ded5132c5d9055d0ed181a83bf516f04132e92
tkatsuki <takeo.katsuki@gmail.com>
2016-07-19 16:45:40 -0700

    Added rolling mean and median for images

--- R/rollmeanimg.R
+++ R/rollmeanimg.R
@@ -1,3 +1,10 @@
+#' Rolling mean calculation for images
+#'
+#' @param obj A target image of Image object or an array.
+#' @param n Window size.
+#' @export
+#' @examples
+#' rollmeanimg()
 rollmeanimg <- function(obj, n){
   rollmeanimgC(obj, dim(obj), n)/n
 }
--- R/rollmedianimg.R
+++ R/rollmedianimg.R
@@ -1,3 +1,10 @@
+#' Rolling median calculation for images
+#'
+#' @param obj A target image of Image object or an array.
+#' @param n Window size.
+#' @export
+#' @examples
+#' rollmedianimg()
 rollmedianimg <- function(obj, n){
   rollmedianimgC(obj, dim(obj), n)/n
 }
