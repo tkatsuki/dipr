e5179a6b3355af893dcc0b0b08391745a63acf37
tkatsuki <takeo.katsuki@gmail.com>
2016-07-19 06:51:30 -0700

    Fixed error

--- R/sfeatures.R
+++ R/sfeatures.R
@@ -11,6 +11,6 @@ sfeatures <- function(obj, ref=0){
   if(length(ref)!=1 & !identical(dim(obj), dim(ref))) stop("Dimension mismatch.")
   width <- dim(obj)[1]
   height <- dim(obj)[2]
-  nframes <- getNumberOfFrames(obj)
+  nframes <- numberOfFrames(obj)
   sfeaturesC(obj, ref, width, height, nframes)
 }
