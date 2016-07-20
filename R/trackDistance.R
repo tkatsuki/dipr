8c1a78a5c5797654826028f31857f41779aa1a81
tkatsuki <takeo.katsuki@gmail.com>
2016-07-20 03:46:23 -0700

    Test doc

--- R/trackDistance.R
+++ R/trackDistance.R
@@ -1,7 +1,7 @@
 #' Template matching
 #'
 #' These functions are for tracking and analyzing objects in movies. tracking calculates various values related to object motion. trackDistance calculates distance traveled by objects during the specified time window. trackSpeed calculates average speed of objects during the specified time window. angularVelocity calculates angular velocity of moving objects. msd calculates mean square diviation of object trajectories.
-#'
+#' @name trackDistance
 #' @param mask	Labeled binary movie.
 #' @param maxdist	Maximum distance for object association.
 #' @param bin Window size for averaging.
@@ -12,10 +12,11 @@
 #' @param dist Traveled distance (e.g. the output of trackDistance).
 #' @param xy A target image of Image object or an array.
 #' @aliases tracking trackSpeed angularVelocity msd
-#' @return Returns a grayscale image.
 #' @export
 #' @examples
 #' tracking()
+NULL
+
 trackDistance <- function (xy, unit = 1) {
   sqrt((head(xy[,1], -1) - xy[-1, 1])^2 + (head(xy[,2], -1) - xy[-1, 2])^2)*unit
 }
@@ -23,8 +24,7 @@ trackDistance <- function (xy, unit = 1) {
 #' @rdname trackDistance
 #' @return Returns a grayscale image.
 #' @export
-#' @examples
-#' tracking()
+
 trackSpeed <- function(dist, bin=5, interval=1){
   v <- c()
   for (i in 1:(length(dist)-bin)){
