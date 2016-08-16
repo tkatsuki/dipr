1d7b613258328928559029c5d3288d32637a18e1
tkatsuki <takeo.katsuki@gmail.com>
2016-08-16 09:18:50 -0700

    Added fmf to tiff converter

--- R/fmf2tif.R
+++ R/fmf2tif.R
@@ -1,9 +1,14 @@
+#' Convert fmf file format to tif format
+#'
+#' @param filename fmf file name
+#' @param skip Skip frames
+#' @export
+#' @examples
+#' fmf2tif()
+#'
 fmf2tif <- function(filename, skip=0){
-  source("C:/Users/GreenspanLab/Dropbox/R/readFMF2.R")
-  require(EBImage)
   print("Converting an fmf file to tif...")
   fmfimg <- readFMF(file=filename, skip=skip)
   prefix <- substr(filename, 1, nchar(filename) - 4)
-  writeImage(fmfimg/255, paste0(prefix, ".tif"))
-  rm(fmfimg)
+  EBImage::writeImage(fmfimg/255, paste0(prefix, ".tif"))
 }
