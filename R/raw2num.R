dbe2ececfa9505abbe8abe401cf995e320ebc22f
tkatsuki <takeo.katsuki@gmail.com>
2016-09-08 06:24:40 -0700

    Added raw2num

--- R/raw2num.R
+++ R/raw2num.R
@@ -1,3 +1,10 @@
+#' raw to integer conversion
+#'
+#' @param rawdata A target image of Image object or an array.
+#' @export
+#' @examples
+#' raw2num()
+
 raw2num <- function(rawdata) {
   return(as.numeric(paste("0x", paste(rawdata, collapse=""), sep="")))
 }
